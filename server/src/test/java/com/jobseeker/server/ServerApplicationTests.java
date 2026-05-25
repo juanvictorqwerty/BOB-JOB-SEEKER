95f283f6d8176075ab206e2cefb181fc53a80776
Juan <juanvictorqwerty@gmail.com>
2026-05-25 23:04:37 +0100

    Database Diagrams

---
+++ server/src/test/java/com/jobseeker/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseeker.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
